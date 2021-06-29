05b50682a9cbe06232dc8bdf4951a2ce745458f6
Aidan Perret <aperret@me.com>
2021-06-29 18:43:04 -0400

    commit tree 595c3e4ee736eb21dfae4e56ac7a92145e455a24

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase())
+    return newDrivers;
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(function(el) {
+        return el.toLowerCase().indexOf(name.toLowerCase()) == 0 
+    })
+}
+    
+function matchName(drivers, name){
+    return drivers.filter(function (hometown) {
+        return hometown.name === name})
+}
